3657b2e9a2496413741ee918e4ff6f4af20aaaa5
mvigant <mvigant@gmail.com>
2015-04-22 20:18:10 +0300

    end

--- app/src/main/java/com/example/mvigant/sharedpreferencessample/MainActivity.java
+++ app/src/main/java/com/example/mvigant/sharedpreferencessample/MainActivity.java
@@ -1,39 +1,84 @@
 package com.example.mvigant.sharedpreferencessample;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private int currentRate;
+    private int maxRate;
+    private TextView  currentRateView;
+    private TextView  maxRateView;
+
+    public static final String PREFS_NAME = "SharedPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // чтение из файла "SharedPreferences";
+        SharedPreferences storeData=getSharedPreferences(PREFS_NAME, 0); // 0 mode private кому доступен файл
+        currentRate=storeData.getInt("currentRate",0);// 0- значение по умолчанию
+        maxRate=storeData.getInt("maxRate",0);// 0- значение по умолчанию
+        //
+        currentRateView= ( TextView)findViewById(R.id.currentRateView);
+        maxRateView= ( TextView)findViewById(R.id.maxRateView);
+        currentRateView.setText(""+currentRate);
+        maxRateView.setText(""+maxRate);
+        //
+       }
+    protected void onStop() {
+        super.onStop();
+        // запись в файл SharedPreferences
+        SharedPreferences storeData=getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor  storeDataEditor=storeData.edit();
+        storeDataEditor.putInt("currentRate",currentRate);// 0- значение по умолчанию
+        storeDataEditor.putInt("maxRate",maxRate);// 0- значение по умолчанию
+        storeDataEditor.commit();
+
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void addCurrentRate(View view) {
+       currentRate++;
+        currentRateView.setText(""+currentRate);
+        if (maxRate  < currentRate ){
+            maxRate=currentRate;
         }
+        maxRateView.setText(""+maxRate);
 
-        return super.onOptionsItemSelected(item);
     }
+    public void clearCurrentRate(View view) {
+        currentRate=0;
+        currentRateView.setText(""+currentRate);
+        // запись в файл SharedPreferences
+        SharedPreferences storeData=getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor  storeDataEditor=storeData.edit();
+        storeDataEditor.putInt("currentRate",currentRate);// 0- значение по умолчанию
+        storeDataEditor.putInt("maxRate",maxRate);// 0- значение по умолчанию
+        storeDataEditor.commit();
+
+    }
+    public void clearAllRate(View view) {
+        currentRate=0;
+        currentRateView.setText(""+currentRate);
+        maxRate=0;
+        maxRateView.setText(""+maxRate);
+        // запись в файл SharedPreferences
+        SharedPreferences storeData=getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor  storeDataEditor=storeData.edit();
+        storeDataEditor.putInt("currentRate",currentRate);// 0- значение по умолчанию
+        storeDataEditor.putInt("maxRate",maxRate);// 0- значение по умолчанию
+        storeDataEditor.commit();
+
+    }
+
+
+  
 }
